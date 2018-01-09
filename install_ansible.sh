8b4bcd2a9186772cf643bdb775a261c4352a91cf
Uros Orozel <uros.orozel@gmail.com>
2018-01-09 23:39:42 +0000

    Added ISO file check, apache2 conf update, proxy support

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,6 @@
 apt-get update
-apt install python-pip
-pip install ansible
+apt install -y  python-pip
+pip install -y  ansible
 ssh-keygen -f ~/.ssh/id_rsa  -t rsa -N ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
